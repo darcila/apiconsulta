cb48a65c9eab03800b6d82052a7eb210c5ec2170
Diego Arcila <diego.arcila@coordinadora.com>
2022-04-20 02:32:58 -0500

    commit inicial

---
+++ consultas/estudiante.py
@@ -0,0 +1,4 @@
+class Estudiante:
+
+    def consultar_evaluaciones(self):
+        return ['curso1', 'curso2', 'curso3']
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from typing import Optional
+from consultas import Estudiante
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
+
+#Peticiones a estudiantes
+@app.get("/estudiante/{correo}")
+def consulta_estudiante(correo: str):
+    consulta = Estudiante()
+    return {"tipo": "peticion get", "correo": correo, "cursos": consulta.consultar_evaluaciones()}
+
+#Peticiones a profesores
+@app.get("/profesor")
+def consulta_profesor():
+    return {"tipo": "peticion get"}
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.get("/test")
+def read_root():
+    return {"nombre": "prueba"}
